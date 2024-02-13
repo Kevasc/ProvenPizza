cf1921bf3361a5bf04586710e247cfa6622d30c0
kevas cole <kevascole@kevass-Air.broadband>
2024-02-13 17:18:18 +0000

    finished page, need to add interactive button elements

--- provenpizza/src/components/footer/Footer.jsx
+++ provenpizza/src/components/footer/Footer.jsx
@@ -1,17 +1,41 @@
 import React from "react";
-import './Footer.css';
+import "./Footer.css";
 
 const Footer = () => {
-    return (
-        <div className="footer-container"> <div className="top-footer">
-        
-        <div className="bottom-footer">
-        <p>PROVEN Pizzeria © Copyright 2022 | Privacy Policy | Creative Brand Partners: Verso Creative</p>
-            
+  return (
+    <div className="footer-container">
+      <div className="top-footer">
+        <div className="alt-logo"></div>
+        <div className="footer-text">
+          <p>
+            PROVEN Pizzeria <br />
+            56 Newcastle Road,
+            <br />
+            Stone, Staffordshire.
+            <br />
+            ST15 8LB
+            <br />
+            T: 01785 817 007
+            <br />
+            <br />
+            dough@provenpizzeria.co.uk
+          </p>
         </div>
-    </div></div>
-       
-    )
-}
+        <div className="follow"> Follow us.</div>
+        <div className="footer-buttons">
+          <div className="footer-button">
+            <p>Book a table</p>
+          </div>
+          <div className="footer-button">
+            <p>Order online</p>
+          </div>
+        </div>
+      </div>
+      <div className="bottom-footer">
+        <p>PROVEN Pizzeria © Copyright 2024 | Privacy Policy | Kevas Cole ☆</p>
+      </div>
+    </div>
+  );
+};
 
 export default Footer;
