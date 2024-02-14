4e1928fa4a09c762cbb8f43a1ded6016c3683360
kevas cole <kevascole@kevass-Air.broadband>
2024-02-14 09:27:50 +0000

    finished! completed ui

--- provenpizza/src/components/header/Header.jsx
+++ provenpizza/src/components/header/Header.jsx
@@ -3,25 +3,26 @@ import logo from "./logo.png";
 import "./Header.css";
 const Header = () => {
   return (
-    
     <div className="heading">
-      <div className='logo-img'><img src={logo} alt="logo" /></div>
-<div className='header-button-container'>
-      <div className="header-button">
-        <p>OUR NEAPOLITAN DOUGH</p>
-      </div>
-      <div className="header-button">
-        <p>WOOD FIRED PIZZA MENU</p>
-      </div>
-      <div className="header-button">
-        <p>FIND US</p>
-      </div>
-      <div className="header-button">
-        <p>ORDER ONLINE</p>
-      </div>
-      <div className="header-button">
-        <p>BOOK A TABLE</p>
-      </div>
+
+        <img className="logo-img" src={logo} alt="logo" />
+ 
+      <div className="header-button-container">
+        <div className="header-button">
+          <p>OUR NEAPOLITAN DOUGH</p>
+        </div>
+        <div className="header-button">
+          <p>WOOD FIRED PIZZA MENU</p>
+        </div>
+        <div className="header-button">
+          <p>FIND US</p>
+        </div>
+        <div className="header-button">
+          <p>ORDER ONLINE</p>
+        </div>
+        <div className="header-button">
+          <p>BOOK A TABLE</p>
+        </div>
       </div>
     </div>
   );
